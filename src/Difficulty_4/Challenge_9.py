1cef88303aeb4a8514a0d3052c6e580c49ed1b69
Brxn <bejiagge@gmail.com>
2023-11-28 09:54:43 +0000

    Update Challenge_9.py

---
+++ src/Difficulty_4/Challenge_9.py
@@ -0,0 +1,13 @@
+firstName = ""
+storedName = "Brandon"
+
+COOL_MSG =  "You're cool."
+OTHER_NAME_MSG = "Nice to meet you."
+
+firstName = input("First name: ")
+
+if firstName == storedName:
+  print(COOL_MSG)
+else:
+  print(OTHER_NAME_MSG)
+
