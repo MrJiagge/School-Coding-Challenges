b78300b11f3b25ae7c910e81250fc71c36428845
Brandon Jiagge <bejiagge@gmail.com>
2023-12-04 21:25:27 +0000

    Update Challenge_11.py

---
+++ src/Difficulty_4/Challenge_11.py
@@ -0,0 +1,12 @@
+num1 = 0
+num2 = 0
+
+num1 = int(input("Number 1: "))
+num2 = int(input("Number 2: "))
+
+if num1 > num2:
+  print(num1)
+elif num1 == num2:
+  print("they are the same")
+else:
+  print(num2)
